8cd0aef2ea566f8876017e33dcfdef3ebb7340e9
Roman Tarasov <portafortuna12@outlook.com>
2020-11-17 23:35:44 +0200

    Added user update password

--- users/serializers.py
+++ users/serializers.py
@@ -9,14 +9,15 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'email', 'first_name', 'last_name', 'role', 'job']
+        fields = ['id', 'email', 'first_name', 'last_name', 'date_of_birth', 'role', 'job']
 
 
-class UserCreateUpdateSerializer(UserSerializer):
+class UserCreateSerializer(UserSerializer):
     confirm_password = serializers.CharField(write_only=True, required=True)
 
     class Meta(UserSerializer.Meta):
         fields = UserSerializer.Meta.fields + ['password', 'confirm_password']
+        extra_kwargs = {'password': {'write_only': True}}
 
     def validate(self, attrs):
         password = attrs.get('password', None)
@@ -29,3 +30,33 @@ class UserCreateUpdateSerializer(UserSerializer):
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
+
+
+class UserUpdatePasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField(write_only=True, required=True)
+    new_password = serializers.CharField(write_only=True, required=True)
+    confirm_password = serializers.CharField(write_only=True, required=True)
+
+    def validate_old_password(self, value):
+        user = self.context.get('request').user
+
+        if not user.check_password(value):
+            raise ValidationError(_('Wrong password'))
+
+        return value
+
+    def validate(self, attrs):
+        old_password = attrs.get('old_password')
+        new_password = attrs.get('new_password')
+
+        if old_password == new_password:
+            raise ValidationError(_('Old and new passwords can not be equal'))
+
+        return attrs
+
+    def save(self, **kwargs):
+        new_password = self.validated_data.get('new_password')
+        user = self.context.get('request').user
+        user.set_password(new_password)
+        user.save()
+        return user
