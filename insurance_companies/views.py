f090601d9d304782f6356c662a7d74b70cf78c20
Roman Tarasov <portafortuna12@outlook.com>
2020-11-18 00:35:49 +0200

    Added views to insurance companies

--- insurance_companies/views.py
+++ insurance_companies/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView
 
-# Create your views here.
+from .models import InsuranceCompany
+from .serializers import InsuranceCompanySerializer
+
+
+User = get_user_model()
+
+
+class InsuranceCompanyCreateAPIView(CreateAPIView):
+    queryset = InsuranceCompany.objects.all()
+    serializer_class = InsuranceCompanySerializer
+
+
+class InsuranceCompanyReadUpdateDeleteAPIVIew(RetrieveUpdateDestroyAPIView):
+    queryset = InsuranceCompany.objects.all()
+    serializer_class = InsuranceCompanySerializer
+
+
+class InsuranceCompanyClientsListAPIView(ListAPIView):
+    serializer_class = InsuranceCompanySerializer
+
+    def get_queryset(self):
+        insurance_company = self.get_object()
+        return User.objects.filter(insurance_company_id=insurance_company.id)
+
+
+class InsuranceCompanyPartnerCompaniesAPIVIew(ListAPIView):
+    serializer_class = InsuranceCompanySerializer
+
+    # def get_queryset(self):
+    #     insurance_company = self.get_object()
+    #     return
+
