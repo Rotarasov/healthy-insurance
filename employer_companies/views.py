1290dbdb017cb17b39b7324571e3e2fa5157f883
Roman Tarasov <portafortuna12@outlook.com>
2020-11-18 01:55:06 +0200

    Added views to employer companies

--- employer_companies/views.py
+++ employer_companies/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, CreateAPIView
 
-# Create your views here.
+from users.models import User, EmployedUser
+from users.serializers import UserCreateSerializer
+from .serializers import EmployerCompanySerializer
+
+
+class EmployeesListAPIVIew(ListAPIView):
+    serializer_class = EmployerCompanySerializer
+
+    def get_queryset(self):
+        employer_company = self.get_object()
+        return EmployedUser.objects.filter(employer_company_id=employer_company.id)
+
+
+class EmployeeCreateAPIVIew(CreateAPIView):
+    serializer_class = UserCreateSerializer
+
+    def create(self, request, *args, **kwargs):
+        request.data['role'] = User.Roles.EMPLOYED
+        return super().create(request, *args, **kwargs)
+
+
+
