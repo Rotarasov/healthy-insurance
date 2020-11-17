726e902eac8966fdbdb23e2fbba78d21e5958538
Roman Tarasov <portafortuna12@outlook.com>
2020-11-18 00:28:57 +0200

    Added insurance company serializer

---
+++ insurance_companies/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from .models import InsuranceCompany
+
+
+class InsuranceCompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = InsuranceCompany
+        fields = '__all__'
