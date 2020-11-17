02a18db82cac3ee180cf0979b35b53ce62af69a8
Roman Tarasov <portafortuna12@outlook.com>
2020-11-17 23:31:48 +0200

    Added base_model check for User in UserManager.get_queryset

--- users/managers.py
+++ users/managers.py
@@ -2,7 +2,7 @@ from django.contrib.auth.base_user import BaseUserManager
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password, first_name, last_name, date_of_birth, **extra_fields):
+    def create_user(self, email, password, first_name, last_name, date_of_birth, role=None, **extra_fields):
         """
         Create and save a User with the given email and password.
         """
@@ -14,14 +14,16 @@ class UserManager(BaseUserManager):
             raise ValueError(_('User must have a last name'))
         if not date_of_birth:
             raise ValueError(_('User must have a date of birth'))
+        if not role:
+            role = self.model.base_role
         email = self.normalize_email(email)
         user = self.model(email=email, first_name=first_name, last_name=last_name,
-                          date_of_birth=date_of_birth, role=self.model.base_role, **extra_fields)
+                          date_of_birth=date_of_birth, role=role, **extra_fields)
         user.set_password(password)
         user.save()
         return user
 
-    def create_superuser(self, email, password, first_name, last_name, date_of_birth, **extra_fields):
+    def create_superuser(self, email, password, first_name, last_name, date_of_birth, role=None, **extra_fields):
         """
         Create and save a SuperUser with the given email and password.
         """
@@ -33,7 +35,11 @@ class UserManager(BaseUserManager):
             raise ValueError(_('Superuser must have is_staff=True.'))
         if extra_fields.get('is_superuser') is not True:
             raise ValueError(_('Superuser must have is_superuser=True.'))
-        return self.create_user(email, password, first_name, last_name, date_of_birth, **extra_fields)
+        return self.create_user(email, password, first_name, last_name, date_of_birth, role, **extra_fields)
 
     def get_queryset(self, *args, **kwargs):
-        return super().get_queryset(*args, **kwargs).filter(role=self.model.base_role)
+        queryset = super().get_queryset(*args, **kwargs)
+        if self.model.base_role == '__all__':
+            return queryset
+        return queryset.filter(role=self.model.base_role)
+
