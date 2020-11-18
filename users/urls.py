ee8832c1e7f1e49b89e25a99d3a929b1d06698f2
Roman Tarasov <portafortuna12@outlook.com>
2020-11-18 12:35:05 +0200

    Deleted update password endpoint

--- users/urls.py
+++ users/urls.py
@@ -6,6 +6,5 @@ app_name = 'users'
 
 urlpatterns = [
     path('<uuid:pk>/', views.UserReadUpdateDeleteAPIView.as_view(), name='detail'),
-    path('', views.UserCreateAPIVIew.as_view(), name='create'),
-    path('update-password/', views.update_user_password, name='update-password')
+    path('', views.UserCreateAPIVIew.as_view(), name='create')
 ]
--- users/views.py
+++ users/views.py
@@ -1,21 +1,10 @@
 from django.contrib.auth import get_user_model
-from rest_framework.decorators import api_view, permission_classes
 from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.response import Response
 
-from .serializers import UserSerializer, UserCreateSerializer, UserUpdatePasswordSerializer
+from .serializers import UserSerializer, UserCreateSerializer
 
 
 User = get_user_model()
-
-
-@api_view(http_method_names=['PUT'])
-# @permission_classes([IsAuthenticated])
-def update_user_password(request, *args, **kwargs):
-    serializer = UserUpdatePasswordSerializer(data=request.data, context={'request': request})
-    serializer.is_valid(raise_exception=True)
-    user_serializer = UserSerializer(serializer.save())
-    return Response(user_serializer.data)
 
 
 class UserCreateAPIVIew(CreateAPIView):
