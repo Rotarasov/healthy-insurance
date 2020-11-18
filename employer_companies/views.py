5bb8f946428bc203e2dc1d6adb7ebf3592d148b2
Roman Tarasov <portafortuna12@outlook.com>
2020-11-18 03:14:03 +0200

    Added view for employee list

--- employer_companies/views.py
+++ employer_companies/views.py
@@ -1,24 +1,15 @@
-from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework.generics import ListAPIView, get_object_or_404
 
-from users.models import User, EmployedUser
-from users.serializers import UserCreateSerializer
-from .serializers import EmployerCompanySerializer
+from users.serializers import UserSerializer
+from .models import EmployerCompany
 
 
 class EmployeesListAPIVIew(ListAPIView):
-    serializer_class = EmployerCompanySerializer
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return get_object_or_404(EmployerCompany, pk=self.kwargs['pk'])
 
     def get_queryset(self):
         employer_company = self.get_object()
-        return EmployedUser.objects.filter(employer_company_id=employer_company.id)
-
-
-class EmployeeCreateAPIVIew(CreateAPIView):
-    serializer_class = UserCreateSerializer
-
-    def create(self, request, *args, **kwargs):
-        request.data['role'] = User.Roles.EMPLOYED
-        return super().create(request, *args, **kwargs)
-
-
-
+        return employer_company.employees
