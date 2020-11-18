e2334d4cd29fabad8132c44db241a11bd7822ddf
Roman Tarasov <portafortuna12@outlook.com>
2020-11-18 10:15:13 +0200

    Integrated EmployedUserCreateSerialzier into EmployeesListAPIVIew

--- employer_companies/views.py
+++ employer_companies/views.py
@@ -1,11 +1,14 @@
-from rest_framework.generics import ListAPIView, get_object_or_404
+from rest_framework.generics import ListCreateAPIView, get_object_or_404
 
-from users.serializers import UserSerializer
+from users.serializers import UserSerializer, EmployedUserCreateSerializer
 from .models import EmployerCompany
 
 
-class EmployeesListAPIVIew(ListAPIView):
-    serializer_class = UserSerializer
+class EmployeesListAPIVIew(ListCreateAPIView):
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return EmployedUserCreateSerializer
+        return UserSerializer
 
     def get_object(self):
         return get_object_or_404(EmployerCompany, pk=self.kwargs['pk'])
