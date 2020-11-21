8aea0befce3465ea81ad49e92f278a3b29a34a39
Roman Tarasov <portafortuna12@outlook.com>
2020-11-21 19:52:58 +0200

    Added changes to measurements fields types

--- users/models.py
+++ users/models.py
@@ -1,10 +1,12 @@
 import uuid
 
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 from django.db import models
 
 from .managers import UserManager
+from healthy_insurance import settings
 
 
 class User(AbstractUser):
@@ -22,11 +24,9 @@ class User(AbstractUser):
     role = models.CharField(_('role'), choices=Roles.choices, max_length=30)
     job = models.CharField(_('job'), max_length=50, blank=True)
     employer_company = models.ForeignKey('employer_companies.EmployerCompany', on_delete=models.CASCADE,
-                                         related_name='employees', related_query_name='employees',
-                                         null=True)
+                                         related_name='employees', null=True)
     insurance_company = models.ForeignKey('insurance_companies.InsuranceCompany', on_delete=models.CASCADE,
-                                          related_name='clients', related_query_name='clients',
-                                          null=True)
+                                          related_name='clients', null=True)
 
     objects = UserManager()
 
@@ -40,6 +40,16 @@ class User(AbstractUser):
 class UnemployedUser(User):
     base_role = User.Roles.UNEMPLOYED
 
+    def clean(self):
+        if self.job:
+            raise ValidationError({'job': _('Unemployed user can not have a job')})
+
+        if self.employer_company:
+            raise ValidationError({'employer_company': _('Unemployed user can not have an employer')})
+
+        if not self.insurance_company:
+            raise ValidationError({'insurance_company': _('Unemployed user must have insurance company')})
+
     class Meta:
         proxy = True
 
@@ -47,10 +57,17 @@ class UnemployedUser(User):
 class EmployedUser(User):
     base_role = User.Roles.EMPLOYED
 
-    def save(self, *args, **kwargs):
-        if not self.pk:
-            self.insurance_company = self.employer_company.insurance_company
-        super().save(*args, **kwargs)
+    def clean(self):
+        if not self.job:
+            raise ValidationError({'job': _('Employed user must have a job')})
+
+        if not self.employer_company:
+            raise ValidationError({'employer_company': _('Employed user must have a employer')})
+
+        if self.insurance_company != self.employer_company.insurance_company:
+            raise ValidationError(
+                {'insurance_company': _('Employee must have insurance company the same as his employer company')}
+            )
 
     class Meta:
         proxy = True
@@ -59,5 +76,32 @@ class EmployedUser(User):
 class EmployerCompanyRepresentative(User):
     base_role = User.Roles.EMPLOYER_COMPANY_REPRESENTATIVE
 
+    def clean(self):
+        if self.job:
+            raise ValidationError({'job': _('Employer company manager can not have a job')})
+
+        if not self.employer_company:
+            raise ValidationError(
+                {'employer_company': _('Employer company representative must have a employer company')}
+            )
+
+        if self.insurance_company != self.employer_company.insurance_company:
+            raise ValidationError(
+                {'insurance_company': _(
+                    'Employer company representative must have insurance company the same as his employer company'
+                )}
+            )
+
     class Meta:
         proxy = True
+
+
+class Measurement(models.Model):
+    id = models.UUIDField(_('id'), primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='measurements')
+    sdnn = models.PositiveIntegerField(_('sdnn'))
+    sdann = models.PositiveIntegerField(_('sdann'))
+    rmssd = models.PositiveIntegerField(_('rmssd'))
+    start = models.DateTimeField(_('start'))
+    end = models.DateTimeField(_('end'))
+
