0923053c5e8e80533fef39a89610a9f2c4c91fca
Roman Tarasov <portafortuna12@outlook.com>
2020-11-25 22:47:13 +0200

    Added insurance price calculation

--- users/services.py
+++ users/services.py
@@ -1,12 +1,14 @@
-from datetime import date
 from math import ceil
+from typing import Dict, List, Union
 
 from django.contrib.auth import get_user_model
+from django.db.models import QuerySet
 
-from .models import Measurement
+from .models import Measurement, InsurancePrice
 
 
 User = get_user_model()
+
 
 NORMAL_HRV_RANGE = {
     10: {'sdnn': {'left': 101, 'right': 279}, 'sdann': {'left': 85, 'right': 261}, 'rmssd': {'left': 25, 'right': 103}},
@@ -20,23 +22,59 @@ NORMAL_HRV_RANGE = {
     90: {'sdnn': {'left': 53, 'right': 147}, 'sdann': {'left': 45, 'right': 140}, 'rmssd': {'left': 7, 'right': 28}},
 }
 
-SALE_COEFFICIENT = 0.8
+
+SDNN_SALE_COEFFICIENT = 0.07
+SDANN_SALE_COEFFICIENT = 0.05
+RMSSD_SALE_COEFFICIENT = 0.03
+
+NUMBER_OF_PREVIOUS_PRICES = 5
+MINIMUM_ACCEPTABLE_RATIO = 0.8
+STABILITY_DISCOUNT_COEFFICIENT = 0.05
 
 
-def calculate_user_insurance_price(user: User) -> int:
-    last_measurement: Measurement = user.measurements.order_by('-end', 'start').first()
-    age = (date.today() - user.date_of_birth).days / 366
-    preprocessed_age = ceil(age / 10.0) * 10
-    normal_hrv_features = NORMAL_HRV_RANGE[preprocessed_age]
-    normal_sdnn = normal_hrv_features['sdnn']
-    normal_sdann = normal_hrv_features['sdann']
-    normal_rmssd = normal_hrv_features['rmssd']
-    old_price = user.insurance_company.individual_price
-    if (normal_sdnn['left'] <= last_measurement.sdnn <= normal_sdnn['right']
-            and normal_sdann['left'] <= last_measurement.sdann <= normal_sdann['right']
-            and normal_rmssd['left'] <= last_measurement.rmssd <= normal_rmssd['right']):
-        return old_price * SALE_COEFFICIENT
-    return old_price
+def calculate_health_discount(measurement: Measurement, normal_hrv_features: Dict[str, Dict[str, int]]) -> float:
+    normal_sdnn_range = normal_hrv_features['sdnn']
+    normal_sdann_range = normal_hrv_features['sdann']
+    normal_rmssd_range = normal_hrv_features['rmssd']
+
+    return (
+            SDNN_SALE_COEFFICIENT * (normal_sdnn_range['left'] <= measurement.sdnn <= normal_sdnn_range['right'])
+            + SDANN_SALE_COEFFICIENT * (normal_sdann_range['left'] <= measurement.sdann <= normal_sdann_range['right'])
+            + RMSSD_SALE_COEFFICIENT * (normal_rmssd_range['left'] <= measurement.rmssd <= normal_rmssd_range['right'])
+    )
+
+
+def calculate_stability_discount(standard_price: int, previous_prices: Union[QuerySet, List[InsurancePrice]]) -> float:
+    total_number = len(previous_prices)
+    total_number_with_discount = sum(map(lambda p: p.price < standard_price, previous_prices))
+
+    if total_number == 0 or total_number_with_discount / total_number < MINIMUM_ACCEPTABLE_RATIO:
+        return 0
+
+    return STABILITY_DISCOUNT_COEFFICIENT
+
+
+def calculate_user_insurance_price(user: User, measurement:Measurement) -> int:
+    round_function = round
+    if user.age % 10 == 5:
+        round_function = ceil
+
+    user_age_category = round_function(user.age / 10) * 10
+    standard_insurance_price = user.insurance_company.individual_price
+
+    normal_hrv_features = NORMAL_HRV_RANGE[user_age_category]
+    health_discount = calculate_health_discount(measurement, normal_hrv_features)
+
+    previous_prices = InsurancePrice.objects.all()[:NUMBER_OF_PREVIOUS_PRICES]
+    stability_discount = calculate_stability_discount(standard_insurance_price, previous_prices)
+
+    total_discount = health_discount + stability_discount
+    return ceil((1 - total_discount) * standard_insurance_price)
+
+
+def create_user_insurance_price(user: User, measurement: Measurement) -> None:
+    price = calculate_user_insurance_price(user, measurement)
+    InsurancePrice.objects.create(user=user, measurement=measurement, price=price)
 
 
 
