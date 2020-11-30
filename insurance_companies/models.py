f766138c3018d9e9af4342a00e631b6e9701c174
Roman Tarasov <portafortuna12@outlook.com>
2020-11-30 20:30:57 +0200

    Added validators for positive numbers

--- insurance_companies/models.py
+++ insurance_companies/models.py
@@ -1,5 +1,6 @@
 import uuid
 
+from django.core.validators import MinValueValidator
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
@@ -9,7 +10,8 @@ class InsuranceCompany(models.Model):
     name = models.CharField(_('name'), max_length=160, unique=True)
     individual_price = models.PositiveIntegerField(_('individual price'))
     family_price = models.PositiveIntegerField(_('family price'))
-    company_sale = models.FloatField(_('company sale'), default=0.1)
-    company_employees_num = models.PositiveIntegerField(_('company employees num'), default=100)
+    company_sale = models.FloatField(_('company sale'), default=0.1, validators=[MinValueValidator(0.)])
+    company_employees_num = models.PositiveIntegerField(_('company employees num'), default=100,
+                                                        validators=[MinValueValidator(0.)])
 
 
