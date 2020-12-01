2875917b0504fbd8bc4f78fdc7ab581b59cb5ba1
Roman Tarasov <portafortuna12@outlook.com>
2020-12-02 00:17:06 +0200

    Added serializer for employer company price

--- employer_companies/serializers.py
+++ employer_companies/serializers.py
@@ -1,11 +1,17 @@
 from rest_framework import serializers
 
-from .models import EmployerCompany, EmployerCompanyCoveragePrice
+from .models import EmployerCompany, EmployerCompanyCoveragePrice, EmployerCompanyPrice
 
 
 class EmployerCompanySerializer(serializers.ModelSerializer):
     class Meta:
         model = EmployerCompany
+        fields = '__all__'
+
+
+class EmployerCompanyPriceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EmployerCompanyPrice
         fields = '__all__'
 
 
