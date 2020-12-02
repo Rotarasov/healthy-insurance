3c69d99b0442689de69c1813a24b884f85d5822f
Roman Tarasov <portafortuna12@outlook.com>
2020-12-02 10:51:31 +0200

    Deleted family members number from employer model

--- employer_companies/models.py
+++ employer_companies/models.py
@@ -9,7 +9,6 @@ class EmployerCompany(models.Model):
     name = models.CharField(_('name'), max_length=160, unique=True)
     industry = models.CharField(_('industry'), max_length=30)
     insurance_coverage = models.FloatField(_('insurance coverage'), max_length=1.0, default=0.72)
-    max_family_members = models.PositiveIntegerField(_('max family members'), default=4)
     insurance_company = models.ForeignKey('insurance_companies.InsuranceCompany', on_delete=models.CASCADE,
                                           related_name='partner_companies', related_query_name='partner_companies')
 
